658141e6a711a1c0ff76629518772f20bac6de16
Jacobgijah <jacobgijjah@gmail.com>
2024-06-24 19:03:03 +0300

    final

--- inventory/settings.py
+++ inventory/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_NAME_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': 'inventorydb',
+        'USER': 'jeco',
+        'PASSWORD': 'jeco',
+        'HOST': 'localhost',
     }
 }
 
